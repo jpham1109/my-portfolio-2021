5de97e63991c844748a145550f880af113275774
Jamie H Pham <jpham1109@gmail.com>
2021-04-26 13:45:06 -0400

    first commit

---
+++ pages/portfolio.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '../styles/Portfolio.module.css'
+
+const portfolio = () => {
+    return (
+        <div className={styles.container}>
+            my portfolio 🦆
+        </div>
+    )
+}
+
+export default portfolio
+
